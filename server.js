c8fa410ebd9f463b265d2e297a6086da4fe65a13
busycodingg <testing9426@gmail.com>
2019-01-15 04:57:39 +0100

    adding server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 const db = knex({
     client: 'pg',
     connection: {
-     host: process.env.DATABASE_URL,
+     connectionString: process.env.DATABASE_URL,
      ssl: true
     }
   });
